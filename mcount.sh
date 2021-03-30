1d51332d0ecf44ff8170936bc2a9c2ab0ed094eb
Justin Clift <justin@postgresql.org>
2021-03-30 11:20:46 +1100

    Add simple test for m at the start of an individual word

--- mcount.sh
+++ mcount.sh
@@ -4,8 +4,23 @@
 # and the count of all words longer than 5 chars.
 
 # Set some initial variables
-NUM_MWORDS=123
+NUM_MWORDS=0
 NUM_LONGWORDS=456
+
+# Test word
+WORD="MOOSE"
+
+# Retrieve the first character in a word
+FIRST_CHAR=`echo ${WORD} | cut -b 1`
+
+# Check if the word started with an 'm'
+if [ $FIRST_CHAR = "m" ]; then
+	let "NUM_MWORDS++"
+elif [ $FIRST_CHAR = "M" ]; then
+	let "NUM_MWORDS++"
+fi
+
+# Count the number of characters in the word
 
 # To Do ...
 
