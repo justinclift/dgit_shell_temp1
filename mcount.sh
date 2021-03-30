5c3ca1689e05447874eb059022676706f6e4a359
Justin Clift <justin@postgresql.org>
2021-03-30 11:32:57 +1100

    Initial loop to process multiple words
    Left debugging stuff and some notes in place, just to show "messy"
    code prior to cleanup.

--- mcount.sh
+++ mcount.sh
@@ -7,27 +7,40 @@
 NUM_MWORDS=0
 NUM_LONGWORDS=0
 
-# Test word
-WORD="MOOSES"
+# Debugging stuff
+echo $@
 
-# Retrieve the first character in a word
-FIRST_CHAR=`echo ${WORD} | cut -b 1`
+# Test words
+WORDS="MOOSES AREN'T GOOSES... or GEESE for that matter"
 
-# Check if the word started with an 'm'
-if [ $FIRST_CHAR = "m" ]; then
-	let "NUM_MWORDS++"
-elif [ $FIRST_CHAR = "M" ]; then
-	let "NUM_MWORDS++"
-fi
+#for WORD in $@
 
-# Count the number of characters in the word
-WORD_LEN=`echo ${WORD} | wc -c`
-let "WORD_LEN--"
+for WORD in $WORDS
+do
 
-# If the word was longer than 5 characters, add it to the word count total
-if [ $WORD_LEN -gt 5 ]; then
-	let "NUM_LONGWORDS++"
-fi
+  # Debugging stuff
+  echo $WORD
+
+  # Retrieve the first character in a word
+  FIRST_CHAR=`echo ${WORD} | cut -b 1`
+
+  # Check if the word started with an 'm'
+  if [ $FIRST_CHAR = "m" ]; then
+    let "NUM_MWORDS++"
+  elif [ $FIRST_CHAR = "M" ]; then
+    let "NUM_MWORDS++"
+  fi
+
+  # Count the number of characters in the word
+  WORD_LEN=`echo ${WORD} | wc -c`
+  let "WORD_LEN--"
+
+  # If the word was longer than 5 characters, add it to the word count total
+  if [ $WORD_LEN -gt 5 ]; then
+    let "NUM_LONGWORDS++"
+  fi
+
+done
 
 # Display the final word count
 echo "Number of words starting with case-insensitive 'm': ${NUM_MWORDS}"
